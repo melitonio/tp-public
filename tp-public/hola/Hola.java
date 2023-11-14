6c017c087416be46bca215cc4f3e3bd97b71a2f4
Melitón Pablo <melitonio8@hotmail.com>
2023-11-14 21:40:06 +0100

    Inicio del respositorio público de la asignatura de Tecnología de la Programación de la UNGE.

---
+++ tp-public/hola/Hola.java
@@ -0,0 +1,8 @@
+public class Hola{
+	public static void main(String[] args){
+		System.out.println("Hola mundo. Este es mi primer programa de Java");
+		if(args.length > 0){
+			System.out.println("El primer argumento es: " + args[0]);
+		}
+	}
+}
